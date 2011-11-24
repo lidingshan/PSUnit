bf5e25b7e6ecfca60eed01200d626eb82540ed51
lidingshan <lidingshan@gmail.com>
2011-11-24 15:48:38 +0800

    add xcopy-exclude-list.txt file to exclude .git directory when xcopy whole things to PSUnit Installer job in Hudson

--- create_installer.bat
+++ create_installer.bat
@@ -2,4 +2,4 @@ devenv Installer\Installer.sln /Build
 
 net use Z: "\\shasrvbsd02\Dataxfer\Mike Li\PSUnit"
 cp Installer\Installer\Bin\PSUnitInstaller.msi Z:
-echo Y | net use Z: /delete
+echo Y|net use Z: /delete
